5cec6bede537a88f21a357581a9342fdfb6431bc
Boros Levente <88634980+FUKUNEPTUN@users.noreply.github.com>
2022-10-01 02:19:14 +0200

    Delete projekt directory

--- projekt/javascript/chemistry/highscores.js
+++
@@ -1,6 +0,0 @@
-const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScoresC')) || []
-highScoresList.innerHTML =
-highScores.map(score=>{
-    return`<li class="high-score" style="font-family: 'Bungee Spice', cursive;;font-size: 2.0rem; margin-bottom: 1.5rem;">${score.name} - ${score.score}</li>`
-}).join('')
